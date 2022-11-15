2e40f3030dc47af5ddb76fd0d03227f376c8f955
Arseniy Usik <senya769@mail.ru>
2022-11-15 20:13:11 +0300

    Initial commit

---
+++ src/main/java/net/backend/questions/softarextask/SoftarexTaskApplication.java
@@ -0,0 +1,13 @@
+package net.backend.questions.softarextask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftarexTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftarexTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/backend/questions/softarextask/SoftarexTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package net.backend.questions.softarextask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftarexTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
