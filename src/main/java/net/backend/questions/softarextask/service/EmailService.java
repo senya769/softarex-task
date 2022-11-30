6d689883dd63ac79cb7aa464e4951b2e010f5794
Arseniy Usik <senya769@mail.ru>
2022-11-30 12:20:27 +0300

    fix

--- src/main/java/net/backend/questions/softarextask/service/EmailService.java
+++ src/main/java/net/backend/questions/softarextask/service/EmailService.java
@@ -1,5 +1,8 @@
 package net.backend.questions.softarextask.service;
 
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
 public interface EmailService {
     void send(String emailTo, String subject, String message);
 }
