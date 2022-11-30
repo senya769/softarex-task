49b402bf099b79614b13393acf6a4a92c41d532e
Arseniy Usik <senya769@mail.ru>
2022-11-30 12:46:07 +0300

    delete args constructor in RequestConfirmPasswordDto

--- src/main/java/net/backend/questions/softarextask/dto/AnswerUpdateDto.java
+++ src/main/java/net/backend/questions/softarextask/dto/AnswerUpdateDto.java
@@ -1,12 +1,10 @@
 package net.backend.questions.softarextask.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
 
 @Data
-@AllArgsConstructor
 public class AnswerUpdateDto {
     @NotNull(message = "must not be empty")
     private String answer;
--- src/main/java/net/backend/questions/softarextask/dto/RequestConfirmPasswordDto.java
+++ src/main/java/net/backend/questions/softarextask/dto/RequestConfirmPasswordDto.java
@@ -1,12 +1,10 @@
 package net.backend.questions.softarextask.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
-@AllArgsConstructor
 public class RequestConfirmPasswordDto {
     @NotBlank(message = "must not be empty")
     private String password;
