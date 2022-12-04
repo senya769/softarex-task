0f09ce4975b0640a1ca0e81328e8b1513432ab40
Arseniy Usik <senya769@mail.ru>
2022-12-04 13:29:06 +0300

    add regexp in validation email and check size password in UserCreateDto

--- src/main/java/net/backend/questions/softarextask/dto/RequestConfirmPasswordDto.java
+++ src/main/java/net/backend/questions/softarextask/dto/RequestConfirmPasswordDto.java
@@ -2,10 +2,12 @@ package net.backend.questions.softarextask.dto;
 
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Data
 public class RequestConfirmPasswordDto {
-    @NotBlank(message = "must not be empty")
+    @NotEmpty(message = "must not be empty")
+    @Size(min = 5, max = 20)
     private String password;
 }
