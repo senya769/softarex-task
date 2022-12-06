0afa3680d211a6bd22885a227c0798ed2642b0ce
Arseniy Usik <senya769@mail.ru>
2022-12-06 19:04:20 +0300

    init

--- src/main/java/net/backend/questions/softarextask/dto/RequestConfirmPasswordDto.java
+++ src/main/java/net/backend/questions/softarextask/dto/RequestConfirmPasswordDto.java
@@ -3,7 +3,6 @@ package net.backend.questions.softarextask.dto;
 import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
 
 @Data
 public class RequestConfirmPasswordDto {
